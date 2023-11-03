181687788e6e44e163409fe441588fd54ef62cf8
Affonso Ruiz <affonsohenriqueruiz@gmail.com>
2023-11-03 10:01:25 -0400

    fix: logout after expire token

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,7 +1,12 @@
-import axios from 'axios';
+import axios, { AxiosError } from 'axios';
 import environment from '../environments';
+import { enqueueSnackbar } from 'notistack';
 
 const token: string | null = localStorage.getItem('token');
+
+let expiredErrorIsActivated = false;
+let messageWasCalled = false;
+
 
 const api = axios.create({
     baseURL: environment.API_URL,
@@ -10,4 +15,26 @@ const api = axios.create({
     }
 });
 
+api.interceptors.response.use(
+    (response) => response,
+    (error: AxiosError) => {
+      if (error.response?.status === 401) {
+        expiredErrorIsActivated = true;
+        if(expiredErrorIsActivated && ! messageWasCalled) {
+            enqueueSnackbar("Tempo de sessão expirado!", {
+              variant: "warning",
+            });
+            messageWasCalled = true;
+        }
+        setTimeout(() => {
+          localStorage.clear();
+          const host = window.location.origin;
+          window.location.replace(host + "/");
+          messageWasCalled = false;
+          expiredErrorIsActivated = false;
+        }, 5000);
+      }
+    }
+  );
+  
 export default api;
