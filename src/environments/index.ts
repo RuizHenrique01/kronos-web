036c63c4fd70276b905075d3437e89c1bac1add0
Affonso Ruiz <affonsohenriqueruiz@gmail.com>
2023-11-04 10:55:43 -0400

    feat: adding grafichs

--- src/environments/index.ts
+++ src/environments/index.ts
@@ -1,4 +1,5 @@
 export default {
     API_URL: import.meta.env.VITE_API_URL || 'http://localhost:4000',
-    KEY_SECRET_ENCRYPT: import.meta.env.VITE_API_KEY_SECRET || 'Kronos@2023'
+    KEY_SECRET_ENCRYPT: import.meta.env.VITE_API_KEY_SECRET || 'Kronos@2023',
+    GRAFANA_HOST: import.meta.env.VITE_GRAFANA_HOST || 'http://localhost:3000'
 }
